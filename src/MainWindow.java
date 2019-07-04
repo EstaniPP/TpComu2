f8811689e16367eff1a39b92f4ddfbc3429722a2
Estani <estanipp@gmail.com>
2019-07-04 14:53:59 -0300

    Interfaz terminada

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -9,14 +9,20 @@ import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class MainWindow {
 
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTable table;
+	private JTable table_1;
 
 	/**
 	 * Launch the application.
@@ -46,7 +52,7 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 699, 502);
+		frame.setBounds(100, 100, 651, 496);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -93,56 +99,49 @@ public class MainWindow {
 		lblNombre.setBounds(12, 44, 79, 15);
 		frame.getContentPane().add(lblNombre);
 		
-		textField = new JTextField();
-		textField.setBounds(96, 42, 95, 19);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
 		JLabel lblRouterNuevo = new JLabel("Router nuevo:");
 		lblRouterNuevo.setBounds(346, 12, 97, 15);
 		frame.getContentPane().add(lblRouterNuevo);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(430, 40, 95, 19);
-		frame.getContentPane().add(textField_1);
 		
 		JLabel label = new JLabel("Nombre:");
 		label.setBounds(346, 42, 79, 15);
 		frame.getContentPane().add(label);
 		
-		JButton btnAplicarAlgoritmo = new JButton("Aplicar algoritmo");
-		btnAplicarAlgoritmo.setBounds(536, 439, 151, 25);
-		frame.getContentPane().add(btnAplicarAlgoritmo);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(12, 228, 322, 197);
-		frame.getContentPane().add(textPane);
-		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setBounds(346, 228, 322, 197);
-		frame.getContentPane().add(textPane_1);
-		
-		JLabel lblRouters = new JLabel("Routers");
+		JLabel lblRouters = new JLabel("Routers:");
 		lblRouters.setBounds(12, 201, 66, 15);
 		frame.getContentPane().add(lblRouters);
 		
-		JLabel lblConexiones = new JLabel("Conexiones");
-		lblConexiones.setBounds(346, 201, 105, 15);
+		JLabel lblConexiones = new JLabel("Conexiones:");
+		lblConexiones.setBounds(150, 201, 105, 15);
 		frame.getContentPane().add(lblConexiones);
+
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Nombre");
 		
-		JButton btnAgregar = new JButton("Agregar entrada");
-		btnAgregar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if(!comboBox.getSelectedItem().equals(comboBox_1.getSelectedItem()) && !textField.getText().equals("")) {
-					if(m.addLink(textField.getText().charAt(0), (int)comboBox_2.getSelectedItem(), (String)comboBox.getSelectedItem(), (String)comboBox_1.getSelectedItem())) {
-						textPane_1.setText(textPane_1.getText()+"Entrada: "+(String)comboBox.getSelectedItem()+ " <---> " +(String)comboBox_1.getSelectedItem() +" L= "+textField.getText().charAt(0)+" C= "+(int)comboBox_2.getSelectedItem() + "\n");
-					}
-				}
-			}
-		});
-		btnAgregar.setBounds(12, 150, 179, 25);
-		frame.getContentPane().add(btnAgregar);
+		DefaultTableModel model2 = new DefaultTableModel();
+		model2.addColumn("Link");
+		model2.addColumn("Costo");
+		model2.addColumn("Router 1");
+		model2.addColumn("Router 2");
+		
+		table = new JTable();
+		table.setCellSelectionEnabled(true);
+		table.setModel(model);
+		
+		table_1 = new JTable();
+		table_1.setCellSelectionEnabled(true);
+		table_1.setModel(model2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 223, 100, 200);
+		frame.getContentPane().add(scrollPane);
+		scrollPane.setViewportView(table);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(150, 223, 480, 200);
+		frame.getContentPane().add(scrollPane_1);
+		scrollPane_1.setViewportView(table_1);
+		
 		
 		JButton btnAgregar_1 = new JButton("Agregar router");
 		btnAgregar_1.addActionListener(new ActionListener() {
@@ -151,11 +150,44 @@ public class MainWindow {
 					if(m.addRouter(textField_1.getText())) {
 						comboBox.addItem(textField_1.getText());
 						comboBox_1.addItem(textField_1.getText());
-						textPane.setText(textPane.getText()+textField_1.getText()+"\n");
+						//textPane.setText(textPane.getText()+textField_1.getText()+"\n");
+						Object[] p = new Object[] {textField_1.getText()};
+						model.addRow(p);
 					}
 			}
 		});
 		btnAgregar_1.setBounds(346, 70, 179, 25);
 		frame.getContentPane().add(btnAgregar_1);
+		
+		
+		JButton btnAgregar = new JButton("Agregar entrada");
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(!comboBox.getSelectedItem().equals(comboBox_1.getSelectedItem()) && !textField.getText().equals("")) {
+					if(m.addLink(textField.getText().charAt(0), (int)comboBox_2.getSelectedItem(), (String)comboBox.getSelectedItem(), (String)comboBox_1.getSelectedItem())) {
+						//textPane_1.setText(textPane_1.getText()+"Entrada: "+(String)comboBox.getSelectedItem()+ " <---> " +(String)comboBox_1.getSelectedItem() +" L= "+textField.getText().charAt(0)+" C= "+(int)comboBox_2.getSelectedItem() + "\n");
+						Object[] p = new Object[] {textField.getText().charAt(0),
+								comboBox_2.getSelectedItem(),
+								comboBox.getSelectedItem(),
+								comboBox_1.getSelectedItem()
+						};
+						model2.addRow(p);
+					}
+				}
+			}
+		});
+		btnAgregar.setBounds(12, 150, 179, 25);
+		frame.getContentPane().add(btnAgregar);
+		
+		JButton btnAplicarAlgoritmo = new JButton("Aplicar algoritmo");
+		btnAplicarAlgoritmo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//m.aplicarAlgoritmo();
+				Visualizador v = new Visualizador(m);
+				v.frame.setVisible(true);
+			}
+		});
+		btnAplicarAlgoritmo.setBounds(479, 428, 151, 25);
+		frame.getContentPane().add(btnAplicarAlgoritmo);
 	}
 }
