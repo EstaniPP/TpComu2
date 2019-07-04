7ac9b9beb0f8793ce715da7c46ffeebe6fe87241
maxi <maxi@maxi-Presario-21>
2019-07-04 09:53:47 -0300

    Clase Link y mod Router

--- src/Router.java
+++ src/Router.java
@@ -1,4 +1,9 @@
+import java.util.HashMap;
+import java.util.List;
 
 public class Router {
-
+	private static int ID_GLOBAL = 1;
+	private int id;
+	private HashMap<Link,Router> adyacentes;
+	private List<Entrada> tablaRuteo; 
 }
