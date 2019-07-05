8357646c631d61042266ed6ad48d79a17ef6b02a
Estani <estanipp@gmail.com>
2019-07-05 13:46:28 -0300

    Interfaz terminada

--- src/Entrada.java
+++ src/Entrada.java
@@ -1,17 +1,17 @@
 
 public class Entrada {
 
-	int destino;
+	String destino;
 	int costo;
 	char link;
 	
-	public Entrada(int destino, int costo, char link) {
+	public Entrada(String destino, int costo, char link) {
 		this.costo=costo;
 		this.destino=destino;
 		this.link=link;
 	}
 	
-	public int getDestino() {
+	public String getDestino() {
 		return destino;
 	}
 	
@@ -31,7 +31,7 @@ public class Entrada {
 		this.costo = costo;
 	}
 	
-	public void setDestino(int destino) {
+	public void setDestino(String destino) {
 		this.destino = destino;
 	}
 	
